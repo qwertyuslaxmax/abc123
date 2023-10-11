5e2dd6dc62d139c9847903f68a7881b5ab062b5f
qwertyuslaxmax <147653359+qwertyuslaxmax@users.noreply.github.com>
2023-10-11 18:35:56 +0000

    meaningful

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+    <script>
+     let thing = "hello"
+    </script>
+
+<div class='a'>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+<div class='d'>
+
+    <h2> {thing} </h2>
+</div>
+
+
+<h3 class='d'>abc</h3>
+<style>
+    .d {
+        color: firebrick;
+    }
+    .a {
+        color: deepskyblue;
+    }
+</style>
