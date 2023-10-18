f0e35fbae1d5b3b23ffa62cfac34ab04ac0d2b33
qwertyuslaxmax <147653359+qwertyuslaxmax@users.noreply.github.com>
2023-10-18 18:11:48 +0000

    meaningful2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
     <script>
-     let thing = "hello"
+     let count = 0
+     function increment(){
+        count += 1
+     }
+     function decrement(){
+        count -= 1
+     }
     </script>
 
 <div class='a'>
-    <h1>Welcome to SvelteKit</h1>
+    <h1> </h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </div>
 <div class='d'>
 
-    <h2> {thing} </h2>
+    <h2> {count} </h2>
 </div>
 
 
@@ -20,4 +26,27 @@
     .a {
         color: deepskyblue;
     }
+
+    .b {
+        padding: 10px;
+        margin: 50px;
+
+        padding-left: 50px;
+    }
 </style>
+<button class="b" on:click={increment}>
+    <p> increment </p>
+    {#if count == 1}
+        <p></p>
+    {:else}
+        <p></p>
+    {/if}
+</button>
+<button class="b" on:click={decrement}>
+    <p> decrement </p>
+    {#if count == 1}
+        <p> </p>
+    {:else}
+        <p> </p>
+    {/if}
+</button>
