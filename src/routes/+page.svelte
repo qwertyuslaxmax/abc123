bde4cf643ed5e6892deda66c84a849cac41912fe
qwertyuslaxmax <147653359+qwertyuslaxmax@users.noreply.github.com>
2023-10-30 03:12:19 +0000

    meaningful

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,148 +1,171 @@
+
 <script lang="ts">
-     let count = 0;
-     let multiplier = 1;
-     function ranDom(max: number){
-        return Math.random() * max | 0;
-     }
-     function increment(){
-        count = count + multiplier;
-     }
-     function decrement(){
-        count = count - multiplier;
-     }
-     function random(){
-        let n = ranDom(2);
-        if(n == 0){
-            count = count + multiplier;
-        } else {
-            count = count - multiplier;
-        }
-     }
+    let count = 0;
+    let multiplier = 1;
+    function displayTextWithDelay() {
+    setTimeout(function() {
+      document.getElementById("game1")!.style.display = "none";
+      document.getElementById("game2")!.style.display = "block";
+    }, 2000);
+    }
+    displayTextWithDelay();
+    function ranDom(max: number){
+       return Math.random() * max | 0;
+    }
+    function increment(){
+       count = count + multiplier;
+    }
+    function decrement(){
+       count = count - multiplier;
+    }
+    function reset1(){
+       count = 0;
+    }
+    function random(){
+       let n = ranDom(2);
+       if(n == 0){
+           count = count + multiplier;
+       } else {
+           count = count - multiplier;
+       }
+    }
+    
+    
 </script>
 <style>
     .parent-container {
-    width: 500px;
-    height: 500px;
-    }
-    .ert{
-        text-align: center;
-        font-size: 1.5rem;
-        background-color: beige;
-    }
-    .d {
-        color: firebrick;
-        margin: 0 auto;
-    }
-    .a {
-        color: rgb(0, 191, 255);
-        margin: 0 auto;
-        
-    }
-    .buttons{
-        font-size: 1.1rem;
-        font-family: Georgia, 'Times New Roman', Times, serif
-    }
-    .abcde{
-    border-color: black;
-    background-color: violet;
-    color: red;
-    padding: 10px;
-    margin: 5px;
-    transition: background-color 0.2s;
-    }
-    .abcde:active,
-    .ab:active {
-    background-color: darkviolet; /* Change the background color when the button is clicked */
-    }
-    .b{
-    border-color:chocolate;
-    background-color: chartreuse;
-    color: green;
-    padding: 10px;
-    margin: 5px;
-    transition: background-color 0.2s;
-    }
-    .ab{
-    border-color:navy;
-    background-color: lavender;
-    color: orange;
-    padding: 10px;
-    margin: 5px;
-    transition: background-color 0.2s;
-    }
-    .zxc{
-        margin: 30px;
-        color:crimson
-    }
-    .parent-container form {
-    margin:20px 0;
-    padding: 20px;
-    background-color: beige;
-    border-radius: 50%;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
+        display: flex; /* Use flexbox layout */
+        justify-content: center; /* Center content horizontally */
+        align-items: center; /* Center content vertically */
+        width: 100%; /* Make the container take up the full width of its parent */
+}
 
-  /* Style the input element inside the circular form */
-  .parent-container form input[type="number"] {
+   .ert{
+       text-align: center;
+       font-size: 1.5rem;
+       background-color: beige;
+   }
+   .d {
+       color: firebrick;
+       margin: 0 auto;
+   }
+   .a {
+       color: rgb(0, 191, 255);
+       margin: 0 auto;
+       
+   }
+   .buttons{
+       font-size: 1.1rem;
+       font-family: Georgia, 'Times New Roman', Times, serif
+   }
+   .abcde{
+   border-color: black;
+   background-color: violet;
+   color: red;
+   padding: 10px;
+   margin: 5px;
+   transition: background-color 0.2s;
+   }
+   .abcde:active,
+   .ab:active {
+   background-color: darkviolet; /* Change the background color when the button is clicked */
+   }
+   .fg:active,
+   .b:active {
+   background-color: tomato; /* Change the background color when the button is clicked */
+   }
+   .b{
+   border-color:chocolate;
+   background-color: chartreuse;
+   color: green;
+   padding: 10px;
+   margin: 5px;
+   transition: background-color 0.2s;
+   }
+   .ab{
+   border-color:navy;
+   background-color: lavender;
+   color: orange;
+   padding: 10px;
+   margin: 5px;
+   transition: background-color 0.2s;
+   }
+   .zxc{
+       margin: 30px;
+       color:crimson
+   }
+   .button-container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+   }
+   .fg{
+    border-color:indigo;
+    background-color:lightslategray; 
+    color: gold;
     padding: 10px;
-    border-radius: 50%;
-    text-align: center; /* Center text horizontally */
-    display: flex;
-    flex-direction: column;
-    align-items: center; /* Center text vertically */
-    justify-content: center;
-  }
+    margin: 5px;
+    transition: background-color 0.2s;
+   }
+   form {
+   margin: 10px 0;
+   padding: 10px;
+   background-color: beige;
+   border: none;
+   border-radius: 15px; /* Increase the border-radius for more rounded corners */
+   }
+
+   form input[type="number"] {
+       padding: 10px;
+       border: 1px solid #999;
+       border-radius: 10px; /* Increase the border-radius for more rounded corners */
+       text-align: center;
+ }
+
 </style>
 
 <div class='ert'>
-    <div class='a'>
-        <h1> Advanced counter </h1>
-    </div>
-    <div class='d'>
-        <h2> {count} </h2>
-        <h2> {multiplier} </h2>
-    </div>
+   <div class='a'>
+       <h1> Advanced counter </h1>
+   </div>
+   <div class='d'>
+       <h2> {count} </h2>
+       <h2> {multiplier} </h2>
+   </div>
 
-    <h3 class='d'>Actions:</h3>
+   <h3 class='d'>Actions:</h3>
 
-    <div class='buttons'>
+   <div class='buttons'>
 
+    <div class="button-container">
         <button class="b" on:click={increment}>
-            <p> Increment </p>
-            {#if count == 1}
-                <p></p>
-            {:else}
-                <p></p>
-            {/if}
+          <p> Increment </p>
         </button>
-
-        <h2 class='zxc'> or </h2>
-
+        <h2 class="zxc"> or </h2>
         <button class="abcde" on:click={decrement}>
-            <p> Decrement </p>
-            {#if count == 1}
-                <p> </p>
-            {:else}
-                <p> </p>
-            {/if}
+          <p> Decrement </p>
         </button>
+        <h2 class='zxc'> or </h2> <!-- Move the "or" text here -->
 
-        <h2 class='zxc'> or </h2>
+       <button class="ab" on:click={random}>
+           <p> Random </p>
+       </button>
+       <h2 class='zxc'> or </h2>
+       <button class="fg" on:click={reset1}>
+           <p> Reset to Zero </p>
+       </button>
 
-        <button class="ab" on:click={random}>
-            <p> Random </p>
-        </button>
-
-        <h2 class='zxc'> or </h2>
-        <div class='parent-container'>
-            <form>
-                <input type="number" placeholder="Multiplier" bind:value={multiplier}>
-            </form>
-        </div>
-
+       
+        
     </div>
+    <h3 class='zxc'>Multiplier:</h3>
+    <div class='parent-container'>
+        <form>
+            <input type="number" placeholder="Multiplier" bind:value={multiplier}>
+        </form>
+    </div>
+    <h1 id="game1">Play Tank Aiming 1 by tadim15 on Scratch</h1>
+    <h1 id="game2">Play Tank Aiming 2 by tadim15 on Scratch</h1>
+
+   </div>
 </div>
